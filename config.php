17085243315b4c180997c87d8334bab497022412
duy hoang <57094089+rimmx@users.noreply.github.com>
2020-08-07 23:52:30 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/** 
- * khai báo các biến cấu hình. 
- 	*/
-define("DB_HOST", "localhost"); 
-define("DB_USER", "root"); 
-define("DB_PASSWORD", ""); 
-define("DB_DATABASE", "db_asm_09296"); 
-?>
